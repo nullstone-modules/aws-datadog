886ddd67818086b3f1c1559d07db4aced6604101
Brad Sickles <brad.sickles@gmail.com>
2021-09-29 17:41:47 -0400

    Added api key secret id to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "api_key_secret_id" {
+  value       = aws_secretsmanager_secret.api_key.id
+  description = "string ||| The ID of the secret containing the datadog API key"
+}
+
 output "api_key_secret_name" {
   value       = aws_secretsmanager_secret.api_key.name
   description = "string ||| The name of the secret containing the datadog API key"
