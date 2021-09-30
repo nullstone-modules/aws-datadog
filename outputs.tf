2ee0ba007f5d1c361fc24830fdff8af147d98fa0
Brad Sickles <brad.sickles@gmail.com>
2021-09-30 14:38:47 -0400

    add outputs for delivery stream

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,18 @@ output "api_key_secret_name" {
   value       = aws_secretsmanager_secret.api_key.name
   description = "string ||| The name of the secret containing the datadog API key"
 }
+
+output "delivery_stream_arn" {
+  value       = aws_kinesis_firehose_delivery_stream.datadog.arn
+  description = "string ||| The ARN of the kinesis firehose delivery stream that will forward to datadog"
+}
+
+output "delivery_role_arn" {
+  value       = aws_iam_role.log_delivery.arn
+  description = "string ||| The ARN of the IAM Role that has permission to deliver logs to the kinesis firehose delivery stream"
+}
+
+output "failed_delivery_bucket_arn" {
+  value       = aws_s3_bucket.failed_log_delivery.arn
+  description = "string ||| The ARN of the S3 bucket where failed log delivery messages are delivered"
+}
